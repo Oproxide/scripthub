aca159f7920956182d391d32024c0b575e15cda3
Oproxide <coopzek@gmail.com>
2025-08-31 13:11:34 -0400

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,8 @@
+local id = game.PlaceId
+function lazy(which)
+  loadstring(game:HttpGet(which))()
+end
+
+if id == 135406051460913 then
+  lazy("https://raw.githubusercontent.com/Oproxide/scripthub/refs/heads/main/runhidefight")
+end
