2987e745e60a1ecff3de7fb63187b4dde2277485
Oproxide <coopzek@gmail.com>
2025-09-01 18:11:45 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,18 +1,16 @@
---[[
-An arrow through the knee, coming for thou'st heart ??? what bruh😭
-My battalion may be plated, So i must control my arm
-www.youtube.com/watch?v=3Th2gEVEhbU
-]]
+-- An arrow through the knee, coming for thou'st heart ??? what bruh😭
+-- My battalion may be plated, So i must control my arm
+-- www.youtube.com/watch?v=3Th2gEVEhbU
 
 local id = game.PlaceId
 function lazy(which)
-  loadstring(game:HttpGet(which))()
+    loadstring(game:HttpGet(which))()
 end
 
 if id == 135406051460913 then
-  lazy("https://raw.githubusercontent.com/Oproxide/scripthub/refs/heads/main/runhidefight")
+    lazy("https://raw.githubusercontent.com/Oproxide/scripthub/refs/heads/main/runhidefight")
 elseif id == 102829972707814 then
-  lazy("https://raw.githubusercontent.com/Oproxide/scripthub/refs/heads/main/civilizationsurvival")
+    lazy("https://raw.githubusercontent.com/Oproxide/scripthub/refs/heads/main/civilizationsurvival")
 else
-  lazy("https://raw.githubusercontent.com/Oproxide/scripthub/refs/heads/main/hub")
+    lazy("https://raw.githubusercontent.com/Oproxide/scripthub/refs/heads/main/hub")
 end
